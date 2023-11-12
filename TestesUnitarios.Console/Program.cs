0127e39fbbfb6b8e912d6be1f69fca7c86faf951
Diego Pablo <diegopmenezes@hotmail.com>
2023-11-12 10:43:21 -0300

    Desafio Testes Unitários usando .NET Core

---
+++ TestesUnitarios.Console/Program.cs
@@ -0,0 +1,98 @@
+﻿using TestesUnitarios.Console;
+
+Calculadora calc = new Calculadora(DateTime.Now.ToString());
+
+int num1 = 0;
+int num2 = 0;
+bool num1Valido = true;
+bool num2Valido = true;
+bool exibirMenu = true;
+
+while (exibirMenu)
+{
+    Console.Clear();
+    num1 = 0;
+    num2 = 0;
+    num1Valido = true; num2Valido = true;
+    Console.WriteLine("Calculadora:");
+    Console.WriteLine("Funções: Somar, Subtrair, Multiplicar, Dividir e Listar Histórico (3 últimas ações do histórico).");
+    Console.WriteLine("Selecione uma das opções logo abaixo:");
+    Console.WriteLine("1 - Somar");
+    Console.WriteLine("2 - Subtrair");
+    Console.WriteLine("3 - Multiplicar");
+    Console.WriteLine("4 - Dividir");
+    Console.WriteLine("5 - Histórico");
+    Console.WriteLine("6 - Encerrar");
+
+
+    switch (Console.ReadLine())
+    {
+        case "1":
+            RecebeDados();
+            if (num1Valido && num2Valido)
+            {
+                Console.WriteLine($"Resultado: {calc.Somar(num1, num2)}");
+            }
+            break;
+
+        case "2":
+            RecebeDados();
+            if (num1Valido && num2Valido)
+            {
+                Console.WriteLine($"Resultado: {calc.Subtrair(num1, num2)}");
+            }
+            break;
+
+        case "3":
+            RecebeDados();
+            if (num1Valido && num2Valido)
+            {
+                Console.WriteLine($"Resultado: {calc.Multiplicar(num1, num2)}");
+            }
+            break;
+
+        case "4":
+            RecebeDados();
+            if (num1Valido && num2Valido)
+            {
+                if (num2 != 0)
+                {
+                    Console.WriteLine($"Resultado: {calc.Dividir(num1, num2)}");
+                }
+                else
+                {
+                    Console.WriteLine($"Resultado: Não é possível dividir por 0!");
+                }
+            }
+            break;
+        case "5":
+            List<string> list = calc.Historico();
+            list.ForEach(x =>
+                Console.WriteLine(x));
+            break;
+        case "6":
+            exibirMenu = false;
+            break;
+
+        default:
+            Console.WriteLine("Opção inválida!");
+            break;
+    }
+    Console.WriteLine("Pressione qualquer tecla para continuar");
+    Console.ReadLine();
+}
+
+void RecebeDados()
+{
+    Console.WriteLine("Informe o primeiro número:");
+    num1Valido = int.TryParse(Console.ReadLine(), out num1);
+    if (num1Valido)
+    {
+        Console.WriteLine("Informe o segundo número:");
+        num2Valido = int.TryParse(Console.ReadLine(), out num2);
+        if (!num2Valido)
+        {
+            Console.WriteLine("número inválido");
+        }
+    }
+}
---
+++ TestesUnitarios.Test/CalculadoraTest.cs
@@ -0,0 +1,92 @@
+using TestesUnitarios.Console;
+
+namespace CalculadoraTest;
+
+public class CalculadoraTest
+{
+    public Calculadora construirClasses()
+    {
+        string data = "12/11/2023";
+        Calculadora calc = new Calculadora(data);
+
+        return calc;
+    }
+    
+    [Theory]
+    [InlineData(1, 2, 3)]
+    [InlineData(4, 5, 9)]
+    public void TesteSomar(int num1, int num2, int resultado)
+    {
+        Calculadora calc = construirClasses();
+
+        int resultadoCalculadora = calc.Somar(num1, num2);
+
+        Assert.Equal(resultado, resultadoCalculadora);
+
+    }
+
+    [Theory]
+    [InlineData(1, 2, 2)]
+    [InlineData(4, 5, 20)]
+    public void TesteMultiplicar(int num1, int num2, int resultado)
+    {
+        Calculadora calc = construirClasses();
+
+        int resultadoCalculadora = calc.Multiplicar(num1, num2);
+
+        Assert.Equal(resultado, resultadoCalculadora);
+
+    }
+
+    [Theory]
+    [InlineData(6, 2, 3)]
+    [InlineData(5, 5, 1)]
+    public void TesteDividir(int num1, int num2, int resultado)
+    {
+        Calculadora calc = construirClasses();
+
+        int resultadoCalculadora = calc.Dividir(num1, num2);
+
+        Assert.Equal(resultado, resultadoCalculadora);
+
+    }
+
+    [Theory]
+    [InlineData(6, 2, 4)]
+    [InlineData(5, 3, 2)]
+    public void TesteSubtrair(int num1, int num2, int resultado)
+    {
+        Calculadora calc = construirClasses();
+
+        int resultadoCalculadora = calc.Subtrair(num1, num2);
+
+        Assert.Equal(resultado, resultadoCalculadora);
+
+    }
+
+    [Fact]
+    public void TestarDivisaoPorZero()
+    {
+        Calculadora calc = construirClasses();
+
+        Assert.Throws<DivideByZeroException>(
+            () => calc.Dividir(3, 0)
+            );
+    }
+
+    [Fact]
+    public void TestarHistorico()
+    {
+        Calculadora calc = construirClasses();
+
+        calc.Somar(1, 2);
+        calc.Subtrair(2, 8);
+        calc.Dividir(3, 7);
+        calc.Multiplicar(4, 1);
+
+        var lista = calc.Historico();
+
+        Assert.NotEmpty(lista);
+        Assert.Equal(3, lista.Count);
+    }
+}
